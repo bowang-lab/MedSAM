cbf49ea0ffbcd3eccfdac0f05789fefe34a689d1
Jun <1259389904@qq.com>
2024-02-19 22:06:52 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     version="0.0.1",
     author="Jun Ma",
     python_requires=">=3.9",
-    install_requires=["monai", "timm", "matplotlib", "scikit-image", "SimpleITK>=2.2.1", "pandas", "nibabel", "tqdm", "scipy", "ipympl", "opencv-python", "jupyterlab", "ipywidgets"],
+    install_requires=["monai", "timm", "matplotlib", "scikit-image", "SimpleITK>=2.2.1", "pyarrow", "pandas", "nibabel", "tqdm", "scipy", "ipympl", "opencv-python", "jupyterlab", "ipywidgets"],
     packages=find_packages(exclude="notebooks"),
     extras_require={
         "all": ["pycocotools", "opencv-python", "onnx", "onnxruntime"],
